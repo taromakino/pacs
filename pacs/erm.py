413032deb3aa257afe88b990eacaffeb80681a4c
makinot1 <makinot1@gene.com>
2023-11-27 10:57:31 -0500

    Log train loss in erm

--- pacs/erm.py
+++ pacs/erm.py
@@ -20,6 +20,7 @@ class ERMBase(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         y_pred, y = self(*batch)
         loss = F.cross_entropy(y_pred, y)
+        self.log('train_loss', loss, on_step=False, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
