a5755b67e5740933c4925312b2f889e042c3e680
makinot1 <makinot1@gene.com>
2023-11-30 11:25:56 -0500

    Freeze BN, and add dropout to ERM

--- pacs/erm.py
+++ pacs/erm.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
@@ -15,15 +16,37 @@ class ERM(pl.LightningModule):
         self.lr = lr
         self.weight_decay = weight_decay
         self.resnet = resnet50(weights='IMAGENET1K_V1')
-        self.resnet.fc = nn.Linear(2048, N_CLASSES)
+        del self.resnet.fc
+        self.dropout = nn.Dropout()
+        self.fc = nn.Linear(2048, N_CLASSES)
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.val_acc = Accuracy('multiclass', num_classes=N_CLASSES)
         self.test_acc = Accuracy('multiclass', num_classes=N_CLASSES)
 
     def forward(self, x, y, e):
-        x = self.normalize(x)
-        y_pred = self.resnet(x)
+        y_pred = self.normalize(x)
+        y_pred = self.resnet.conv1(y_pred)
+        y_pred = self.resnet.bn1(y_pred)
+        y_pred = self.resnet.relu(y_pred)
+        y_pred = self.resnet.maxpool(y_pred)
+
+        y_pred = self.resnet.layer1(y_pred)
+        y_pred = self.resnet.layer2(y_pred)
+        y_pred = self.resnet.layer3(y_pred)
+        y_pred = self.resnet.layer4(y_pred)
+
+        y_pred = self.resnet.avgpool(y_pred)
+        y_pred = torch.flatten(y_pred, 1)
+
+        y_pred = self.dropout(y_pred)
+        y_pred = self.fc(y_pred)
         return y_pred, y
+
+    def on_train_start(self):
+        for module in self.resnet.modules():
+            if isinstance(module, nn.BatchNorm2d):
+                module.requires_grad_(False)
+                module.eval()
 
     def training_step(self, batch, batch_idx):
         y_pred, y = self(*batch)
