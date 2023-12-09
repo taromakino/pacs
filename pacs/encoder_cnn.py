e218f63424a0e810f6209d502d7caf656178e4c3
makinot1 <makinot1@gene.com>
2023-12-09 16:55:14 -0500

    Minor changes

--- pacs/encoder_cnn.py
+++ pacs/encoder_cnn.py
@@ -10,9 +10,9 @@ class EncoderCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.resnet = resnet50(weights='IMAGENET1K_V1')
-        del self.resnet.classifier
-        self.resnet.classifier = nn.Identity()
+        del self.resnet.fc
+        self.resnet.fc = nn.Identity()
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     def forward(self, x):
-        return self.densenet(self.normalize(x))
+        return self.resnet(self.normalize(x))
