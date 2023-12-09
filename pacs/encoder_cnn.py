b0cbc6f66ead900fac4359835bca8ee93bd49edb
makinot1 <makinot1@gene.com>
2023-12-09 16:47:26 -0500

    ResNet50

--- pacs/encoder_cnn.py
+++ pacs/encoder_cnn.py
@@ -1,17 +1,17 @@
 import torch.nn as nn
 import torchvision.transforms as transforms
-from torchvision.models import densenet121
+from torchvision.models import resnet50
 
 
-IMG_ENCODE_SIZE = 1024
+IMG_ENCODE_SIZE = 2048
 
 
 class EncoderCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.densenet = densenet121(weights='IMAGENET1K_V1')
-        del self.densenet.classifier
-        self.densenet.classifier = nn.Identity()
+        self.resnet = resnet50(weights='IMAGENET1K_V1')
+        del self.resnet.classifier
+        self.resnet.classifier = nn.Identity()
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     def forward(self, x):
