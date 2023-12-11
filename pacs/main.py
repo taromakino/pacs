2208c946201004b35411db689f63287d0f454953
makinot1 <makinot1@gene.com>
2023-12-11 16:10:11 -0500

    save_on_train_epoch_end

--- pacs/main.py
+++ pacs/main.py
@@ -67,7 +67,7 @@ def main(args):
         trainer = pl.Trainer(
             logger=CSVLogger(os.path.join(args.dpath, args.task.value), name='', version=args.seed),
             callbacks=[
-                ModelCheckpoint(save_last=True)],
+                ModelCheckpoint(save_last=True, save_on_train_epoch_end=True)],
             max_epochs=args.n_epochs,
             check_val_every_n_epoch=args.check_val_every_n_epoch,
             num_sanity_val_steps=0,
