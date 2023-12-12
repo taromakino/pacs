be5b718476af03728c6db25270157f7739a3dda9
makinot1 <makinot1@gene.com>
2023-12-11 21:46:54 -0500

    Minor changes

--- pacs/main.py
+++ pacs/main.py
@@ -108,6 +108,6 @@ if __name__ == '__main__':
     parser.add_argument('--weight_decay', type=float, default=1e-5)
     parser.add_argument('--lr_infer', type=float, default=1)
     parser.add_argument('--n_infer_steps', type=int, default=200)
-    parser.add_argument('--n_epochs', type=int, default=1000)
-    parser.add_argument('--check_val_every_n_epoch', type=int, default=100)
+    parser.add_argument('--n_epochs', type=int, default=200)
+    parser.add_argument('--check_val_every_n_epoch', type=int, default=20)
     main(parser.parse_args())
