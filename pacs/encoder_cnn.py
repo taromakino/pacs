244a7b016a51477ccefb8f5c47bf4a5fd425c3d4
makinot1 <makinot1@gene.com>
2023-12-13 23:47:06 -0500

    regnet_x_400mf

--- pacs/encoder_cnn.py
+++ pacs/encoder_cnn.py
@@ -1,18 +1,18 @@
 import torch.nn as nn
 import torchvision.transforms as transforms
-from torchvision.models import resnet50
+from torchvision.models.regnet import regnet_x_400mf
 
 
-IMG_ENCODE_SIZE = 2048
+IMG_ENCODE_SIZE = 400
 
 
 class EncoderCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.resnet = resnet50(weights='IMAGENET1K_V1')
-        del self.resnet.fc
-        self.resnet.fc = nn.Identity()
+        self.net = regnet_x_400mf()
+        del self.net.fc
+        self.net.fc = nn.Identity()
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     def forward(self, x):
-        return self.resnet(self.normalize(x))
+        return self.net(self.normalize(x))
